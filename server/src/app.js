24be6deec18245bb72b85a540bff0ba0c7767956
aharon weber <a.w770770@gmail.com>
2026-08-22 22:12:06 +0300

    add: cors support for developing the client with live server

--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,12 @@
 import express from "express"
 import gameRouter from "./routes/game.routes.js"
+import cors from "cors"
 
 export const app = express()
 
+app.use(cors())
 app.use(express.json())
-// app/use(express.static())
+app.use(express.static("../client"))
 app.use("", gameRouter)
 app.use((err, req, res, next) => {
   res.status(err.status || 500).json({ error: err.message });
